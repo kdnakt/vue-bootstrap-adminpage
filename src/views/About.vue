934d7538f09cb252c97d0623f8d03e368162ccf3
kdnakt <a.kid.1985@gmail.com>
2022-01-23 14:55:30 +0900

    add Logo component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
+    <Logo />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Logo from '@/components/Logo.vue';
+
+@Options({
+  components: {
+    Logo,
+  },
+})
+export default class About extends Vue {}
+</script>
--- src/views/Dummy.vue
+++ src/views/Dummy.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
+    <Logo />
     <h2>This is a dummy page</h2>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Logo from '@/components/Logo.vue';
+
+@Options({
+  components: {
+    Logo,
+  },
+})
+export default class Dummy extends Vue {}
+</script>
